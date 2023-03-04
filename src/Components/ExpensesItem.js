eb5f17d8bb6d63253c820b958035628e01defb62
GBoshnakov <boshnakov.g@gmail.com>
2023-03-04 12:31:50 +0200

    css added

--- src/Components/ExpensesItem.js
+++ src/Components/ExpensesItem.js
@@ -1,5 +1,16 @@
-function Expenses () {
-    return <h2>test</h2>
+import './ExpensesItem.css';
+
+function Expenses() {
+    return (
+        <div className='expense-item'>
+            <div>
+                March 4th
+            </div>
+            <div className='expense-item__description'>
+                <h2>Car Insurance</h2>
+                <div className='expense-item__price'>$280</div>
+            </div>
+        </div>)
 }
 
 export default Expenses;
