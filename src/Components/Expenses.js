2eba53961e33571e9b80328e3af54e13e08ac40d
GBoshnakov <boshnakov.g@gmail.com>
2023-03-05 17:11:51 +0200

    wrapper added

--- src/Components/Expenses.js
+++ src/Components/Expenses.js
@@ -1,16 +1,17 @@
 import './Expenses.css';
 import ExpensesItem from './ExpensesItem';
+import Card from './Card';
 
 function Expenses(props) {
 
     const data = props.data;
     return (
-        <div className='expenses'>
+        <Card className='expenses'>
             <ExpensesItem title={data[0].title} date={data[0].date} amount={data[0].amount}></ExpensesItem>
             <ExpensesItem title={data[1].title} date={data[1].date} amount={data[1].amount}></ExpensesItem>
             <ExpensesItem title={data[2].title} date={data[2].date} amount={data[2].amount}></ExpensesItem>
             <ExpensesItem title={data[3].title} date={data[3].date} amount={data[3].amount}></ExpensesItem>
-        </div>
+        </Card>
     )
 }
 
