d00a6b5201b4e5ad1700352eee0d5a4775093b49
GBoshnakov <boshnakov.g@gmail.com>
2023-03-06 20:14:30 +0200

    event listener added

--- src/Components/ExpensesItem.js
+++ src/Components/ExpensesItem.js
@@ -1,16 +1,26 @@
+import { useState } from 'react';
+
 import './ExpensesItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
 
 function ExpensesItem(props) {
 
+    const [title, setTitle] = useState(props.title);
+
+    function eventHandler() {
+        console.log(title)
+        setTitle('Changed!');
+    }
+
 
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
+                <button onClick={eventHandler}>Change title</button>
             </div>
         </Card>)
 }
