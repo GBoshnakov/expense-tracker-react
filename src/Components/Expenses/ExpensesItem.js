90ccb12a71c17e2369660ea4cf55e636106e9852
GBoshnakov <boshnakov.g@gmail.com>
2023-03-07 19:51:44 +0200

    folder refactoring

--- src/Components/Expenses/ExpensesItem.js
+++ src/Components/Expenses/ExpensesItem.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 import './ExpensesItem.css';
 import ExpenseDate from './ExpenseDate';
-import Card from './Card';
+import Card from '../UI/Card';
 
 function ExpensesItem(props) {
 
