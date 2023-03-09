a894e662a88b2784b2c5df87ed03c2a017213f28
GBoshnakov <boshnakov.g@gmail.com>
2023-03-09 23:34:48 +0200

    state exercise

--- src/Components/NewExpense/NewExpenseForm.js
+++ src/Components/NewExpense/NewExpenseForm.js
@@ -2,22 +2,41 @@ import React from "react";
 import './NewExpenseForm.css';
 
 function NewExpenseForm () {
+
+    const [title, setTitle] = React.useState('');
+    const [amount, setAmount] = React.useState('');
+    const [date, setDate] = React.useState('');
+
+
+    function titleChangeHandler(event) {
+        setTitle(event.target.value);
+    }
+
+    function amountChangehandler(event) {
+        setAmount(event.target.value);
+    }
+
+    function dateChangeHandler(event) {
+        setDate(event.target.value);
+    }
+
+
     return (
         <form>
             <div className="new-expense__control">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type='text'/>
+                    <input type='text' onChange={titleChangeHandler}/>
                 </div>
 
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type='number' min='0.01' step='0.01'/>
+                    <input type='number' min='0.01' step='0.01' onChange={amountChangehandler}/>
                 </div>
 
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type='date' min='2000-01-01' max='2025-01-01' />
+                    <input type='date' min='2000-01-01' max='2025-01-01'onChange={dateChangeHandler} />
                 </div>
 
             </div>
